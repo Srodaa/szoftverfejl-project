5c04b6affc516b80fb0a50a2be52345d2db0160c
Szabó Dávid <sroda@gmailbox.unideb.hu>
2024-05-08 15:46:26 +0200

    build: alap JavaFX felület létrehozva

---
+++ src/main/java/boardgame/BoardGameApplication.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BoardGameApplication extends Application {
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/boardGame.fxml"));
+        stage.setTitle("JavaFX Logic Board Game");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+    }
+}
