c5906a14991948bddd9db312f935b39975182f09
Szabó Dávid <david030719@gmail.com>
2024-05-23 11:49:04 +0200

    docs: gui.BoardGameApplication osztály dokumentálva

--- src/main/java/boardgame/gui/BoardGameApplication.java
+++ src/main/java/boardgame/gui/BoardGameApplication.java
@@ -8,7 +8,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * The main class of the application.
+ */
 public class BoardGameApplication extends Application {
+    /**
+     * The entry point of the application.
+     *
+     * @param stage the primary stage for this application, onto which the application scene can be set.
+     * @throws IOException if an error occurs during the loading of the FXML file.
+     */
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/startScene.fxml"));
         stage.setTitle("JavaFX Logic Board Game");
