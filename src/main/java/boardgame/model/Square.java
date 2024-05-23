f797bca3c7e42b1700b9925fe4afe116bb05a3f5
Szabó Dávid <david030719@gmail.com>
2024-05-23 11:31:28 +0200

    docs: model.Square osztály dokumentálva

--- src/main/java/boardgame/model/Square.java
+++ src/main/java/boardgame/model/Square.java
@@ -5,8 +5,20 @@ package boardgame.model;
  * The board can contain a king, a bishop, a rook, or no piece.
  */
 public enum Square {
+    /**
+     * The {@code NONE} represents an empty square.
+     */
     NONE,
+    /**
+     * The {@code KING} represents a king piece.
+     */
     KING,
+    /**
+     * The {@code BISHOP} represents a bishop piece.
+     */
     BISHOP,
+    /**
+     * The {@code ROOK} represents a rook piece.
+     */
     ROOK
 }
